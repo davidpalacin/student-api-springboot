3515c4f41bf7e32337e078355ccd7f36a7774c1b
david <palacingimenezdavid.fpllefia@gmail.com>
2023-08-20 15:31:17 +0200

    first commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+  public List<Student> getStudents() {
+    return List.of(
+        new Student(
+            1L,
+            "David",
+            "palacingimenezdavid.fpllefia@gmail.com",
+            LocalDate.of(1999, Month.JULY, 12),
+            24));
+  }
+}
